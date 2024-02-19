c3c0f61148efda5baa0d9ea14850139e766c0961
Brian Takita <brian.takita@gmail.com>
2024-02-18 22:09:19 -0500

    ./anchor: target_blank__a_: + nofollow?:boolean

--- anchor/target_blank__a.ts
+++ anchor/target_blank__a.ts
@@ -1,4 +1,4 @@
-import { class_ } from 'ctx-core/html'
+import { attr_val_, class_ } from 'ctx-core/html'
 import { raw_, type relement_env_T, switch_, type tag_dom_T } from 'relementjs'
 import { type tag_props_T } from 'relementjs/any'
 import { a_ } from 'relementjs/html'
@@ -14,7 +14,11 @@ export function target_blank__a_<env_T extends relement_env_T>(
 				'a_target_blank',
 				'whitespace-nowrap',
 				$p.class),
-			rel: $p.rel ?? 'noopener noreferrer',
+			rel:
+				$p.rel
+				?? attr_val_(
+					'noopener noreferrer',
+					$p.nofollow ? 'nofollow' : undefined),
 			target: '_blank',
 		}, [
 			switch_({
@@ -41,15 +45,16 @@ export function target_blank__a_<env_T extends relement_env_T>(
 					'ml-1',
 					'inline-block'),
 				viewBox: '0 0 16 16'
-			},
-			path_({
-				'fill-rule': 'evenodd',
-				d: 'M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z'
-			}),
-			path_({
-				'fill-rule': 'evenodd',
-				d: 'M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z'
-			}))
+			}, [
+				path_({
+					'fill-rule': 'evenodd',
+					d: 'M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z'
+				}),
+				path_({
+					'fill-rule': 'evenodd',
+					d: 'M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z'
+				})
+			])
 		])
 	)
 }
@@ -60,6 +65,7 @@ export type target_blank_a__props_T = {
 	innerHTML?:string
 	class?:string
 	style?:string
+	nofollow?:boolean
 	rel?:string
 }&tag_props_T<HTMLAnchorElement>
 
