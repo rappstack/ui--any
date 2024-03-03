da6d5c231b4aba4d25439820c112e5f7b7daf866
Brian Takita <brian.takita@gmail.com>
2024-03-02 19:58:52 -0500

    ./md:
    	+ hljs.css: highlight.js/styles/panda-syntax-dark.css
    	md__raw_:
    		∋ markedHighlight
    		∋ highlight.js
    	∋ highlight.js
    	∋ marked-highlight

--- md/md__raw.ts
+++ md/md__raw.ts
@@ -1,5 +1,26 @@
-import { marked } from 'marked'
+import './hljs.css'
+import hljs from 'highlight.js'
+import cs from 'highlight.js/lib/languages/csharp'
+import js from 'highlight.js/lib/languages/javascript'
+import sh from 'highlight.js/lib/languages/shell'
+import ts from 'highlight.js/lib/languages/typescript'
+import { Marked } from 'marked'
+import { markedHighlight } from 'marked-highlight'
 import { raw_ } from 'relementjs'
+hljs.registerLanguage('cs', cs)
+hljs.registerLanguage('js', js)
+hljs.registerLanguage('sh', sh)
+hljs.registerLanguage('shell', sh)
+hljs.registerLanguage('ts', ts)
+const marked = new Marked(
+	markedHighlight({
+		langPrefix: 'hljs language-',
+		highlight(code, lang, info) {
+			const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+			return hljs.highlight(code, { language }).value;
+		}
+	})
+)
 export function md__raw_(md:string) {
 	return raw_(marked.parse(md))
 }
