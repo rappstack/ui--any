8dcc3e04db5d455474d374c78194122af1ab50fd
Brian Takita <brian.takita@gmail.com>
2024-04-28 22:28:19 -0400

    ./dl:
    	dl_dt_dd_tree_props_T→dl_tree_props_T
    	dl_dt_dd_tree_→dl_tree_
    	+ dt_md_
    	+ dd_md_

--- dl/index.ts
+++ dl/index.ts
@@ -3,31 +3,31 @@ import { type tag_dom_T, type wide_ctx_T } from 'relementjs'
 import { type tag_props_T } from 'relementjs/any'
 import { dd_, dl_, dt_ } from 'relementjs/html'
 import { md__raw_ } from '../md/index.js'
-export type dl_dt_dd_tree_props_T = {
+export type dl_tree_props_T = {
 	ctx:wide_ctx_T
 	dl_class?:string
 	dt_class?:string
 	dd_class?:string
 }
-export function dl_dt_dd_tree_(
-	pair_a1:pair_a1_T
+export function dl_tree_(
+	dt_dd_pair_a1:dt_dd_pair_a1_T
 ):tag_dom_T
-export function dl_dt_dd_tree_(
-	props:dl_dt_dd_tree_props_T,
-	pair_a1:pair_a1_T
+export function dl_tree_(
+	props:dl_tree_props_T,
+	dt_dd_pair_a1:dt_dd_pair_a1_T
 ):tag_dom_T
-export function dl_dt_dd_tree_(
-	props_OR_pair_a1:dl_dt_dd_tree_props_T|pair_a1_T,
-	pair_a1?:pair_a1_T
+export function dl_tree_(
+	props_OR_dt_dd_pair_a1:dl_tree_props_T|dt_dd_pair_a1_T,
+	dt_dd_pair_a1?:dt_dd_pair_a1_T
 ) {
-	if ((<dl_dt_dd_tree_props_T>props_OR_pair_a1).ctx.is_ctx) {
+	if ((<dl_tree_props_T>props_OR_dt_dd_pair_a1).ctx.is_ctx) {
 		const {
 			ctx,
 			dl_class,
 			dt_class,
 			dd_class,
-		} = <dl_dt_dd_tree_props_T>props_OR_pair_a1
-		return dl_({ class: dl_class }, pair_a1!.map(([_dt, _dd])=>[
+		} = <dl_tree_props_T>props_OR_dt_dd_pair_a1
+		return dl_({ class: dl_class }, dt_dd_pair_a1!.map(([_dt, _dd])=>[
 			dt_(
 				{ class: dt_class },
 				typeof _dt === 'string'
@@ -42,8 +42,8 @@ export function dl_dt_dd_tree_(
 				: null,
 		]))
 	} else {
-		pair_a1 = <pair_a1_T>props_OR_pair_a1
-		return dl_(pair_a1.map(([_dt, _dd])=>[
+		dt_dd_pair_a1 = <dt_dd_pair_a1_T>props_OR_dt_dd_pair_a1
+		return dl_(dt_dd_pair_a1.map(([_dt, _dd])=>[
 			dt_(_dt),
 			_dd
 				? dd_(_dd)
@@ -51,4 +51,18 @@ export function dl_dt_dd_tree_(
 		]))
 	}
 }
-type pair_a1_T = ([tag_dom_T, tag_dom_T|[tag_props_T<HTMLElement>, tag_dom_T]]|[tag_dom_T])[]
+type dt_dd_pair_a1_T = ([tag_dom_T, tag_dom_T|[tag_props_T<HTMLElement>, tag_dom_T]]|[tag_dom_T])[]
+export function dt_md_(
+	$p:tag_props_T<HTMLElementTagNameMap['dt']>&{ ctx:wide_ctx_T },
+	md:string
+) {
+	const { ctx, ...props } = $p
+	return dt_(props, md__raw_(ctx, md))
+}
+export function dd_md_(
+	$p:tag_props_T<HTMLElementTagNameMap['dd']>&{ ctx:wide_ctx_T },
+	md:string
+) {
+	const { ctx, ...props } = $p
+	return dd_(props, md__raw_(ctx, md))
+}
