698d14c0bf4906994a1d8f3d209219078e4f610a
Brian Takita <brian.takita@gmail.com>
2024-05-01 00:16:45 -0400

    ./dl:
    	dl_tree_: dt_dd_pair_a1_ argument: (level:number)=>dt_dd_pair_a1_T
    	dl_tree_props_T: + _?:(dl_tree_level:number)=>{ dl_class?:string, dt_class?:string, dd_class?:string }

--- dl/index.ts
+++ dl/index.ts
@@ -1,4 +1,5 @@
 /// <reference lib="dom" />
+import { class_ } from 'ctx-core/html'
 import { type relement_env_T, type tag_dom_T, type wide_ctx_T } from 'relementjs'
 import { type tag_props_T } from 'relementjs/any'
 import { dd_, div_, dl_, dt_ } from 'relementjs/html'
@@ -8,54 +9,72 @@ export type dl_tree_props_T = {
 	dl_class?:string
 	dt_class?:string
 	dd_class?:string
+	_?:(dl_tree_level:number)=>{
+		dl_class?:string
+		dt_class?:string
+		dd_class?:string
+	}
 }
+let dl_tree_level = -1
 export function dl_tree_<env_T extends relement_env_T>(
-	dt_dd_pair_a1_:()=>dt_dd_pair_a1_T
+	dt_dd_pair_a1_:(level:number)=>dt_dd_pair_a1_T
 ):tag_dom_T
 export function dl_tree_<env_T extends relement_env_T>(
 	props:dl_tree_props_T,
-	dt_dd_pair_a1_:()=>dt_dd_pair_a1_T
+	dt_dd_pair_a1_:(level:number)=>dt_dd_pair_a1_T
 ):tag_dom_T
 export function dl_tree_<env_T extends relement_env_T>(
-	props_OR_dt_dd_pair_a1_:dl_tree_props_T|(()=>dt_dd_pair_a1_T),
-	dt_dd_pair_a1_?:()=>dt_dd_pair_a1_T
+	props_OR_dt_dd_pair_a1_:dl_tree_props_T|((level:number)=>dt_dd_pair_a1_T),
+	dt_dd_pair_a1_?:(level:number)=>dt_dd_pair_a1_T
 ) {
-	if ((<dl_tree_props_T>props_OR_dt_dd_pair_a1_).ctx?.is_ctx) {
-		const {
-			ctx,
-			dl_class,
-			dt_class,
-			dd_class,
-		} = <dl_tree_props_T>props_OR_dt_dd_pair_a1_
-		return dl_<env_T>({ class: dl_class }, dt_dd_pair_a1_!().map(([_dt, _dd])=>{
-			const _dt_a1 = <tag_dom_T[]>(<any[]>[_dt]).flat(Infinity)
-			const _dd_a1 = _dd ? <tag_dom_T[]>(<any[]>[_dd]).flat(Infinity) : null
-			return [
-				div_({ class: 'relative' }, [
-					dt_({ class: dt_class },
-						_dt_a1.map(__dt=>
-							typeof __dt === 'string'
-								? md__raw_(ctx, __dt)
-								: __dt)),
-					_dd
-						? dd_(
-							{ class: dd_class },
-							_dd_a1!.map(__dd=>
-								typeof __dd === 'string'
-									? md__raw_(ctx, __dd)
-									: __dd))
-						: null,
-				]),
-			]
-		}))
-	} else {
-		dt_dd_pair_a1_ = <()=>dt_dd_pair_a1_T>props_OR_dt_dd_pair_a1_
-		return dl_<env_T>(dt_dd_pair_a1_().map(([_dt, _dd])=>[
-			dt_(_dt),
-			_dd
-				? dd_(_dd)
-				: null,
-		]))
+	++dl_tree_level
+	try {
+		if ((<dl_tree_props_T>props_OR_dt_dd_pair_a1_).ctx?.is_ctx) {
+			let {
+				ctx,
+				dl_class,
+				dt_class,
+				dd_class,
+				_
+			} = <dl_tree_props_T>props_OR_dt_dd_pair_a1_
+			if (_) {
+				const props2 = _(dl_tree_level)
+				dl_class = class_(dl_class, props2.dl_class)
+				dt_class = class_(dt_class, props2.dt_class)
+				dd_class = class_(dd_class, props2.dd_class)
+			}
+			return dl_<env_T>({ class: dl_class }, dt_dd_pair_a1_!(dl_tree_level).map(([_dt, _dd])=>{
+				const _dt_a1 = <tag_dom_T[]>(<any[]>[_dt]).flat(Infinity)
+				const _dd_a1 = _dd ? <tag_dom_T[]>(<any[]>[_dd]).flat(Infinity) : null
+				return [
+					div_([
+						dt_({ class: dt_class },
+							_dt_a1.map(__dt=>
+								typeof __dt === 'string'
+									? md__raw_(ctx, __dt)
+									: __dt)),
+						_dd
+							? dd_(
+								{ class: dd_class },
+								_dd_a1!.map(__dd=>
+									typeof __dd === 'string'
+										? md__raw_(ctx, __dd)
+										: __dd))
+							: null,
+					]),
+				]
+			}))
+		} else {
+			dt_dd_pair_a1_ = <()=>dt_dd_pair_a1_T>props_OR_dt_dd_pair_a1_
+			return dl_<env_T>(dt_dd_pair_a1_(dl_tree_level).map(([_dt, _dd])=>[
+				dt_(_dt),
+				_dd
+					? dd_(_dd)
+					: null,
+			]))
+		}
+	} finally {
+		--dl_tree_level
 	}
 }
 type dt_dd_pair_a1_T = ([tag_dom_T, tag_dom_T]|[tag_dom_T])[]
