a6d3c8a41122dae8333aca57d392562e7d9988ca
Brian Takita <brian.takita@gmail.com>
2024-05-01 15:35:56 -0400

    ./dl: dl_tree_props_T:
    	+ hyop?:string
    	+ div_class?:string

--- dl/index.ts
+++ dl/index.ts
@@ -1,4 +1,5 @@
 /// <reference lib="dom" />
+import { div } from 'ctx-core/all'
 import { class_ } from 'ctx-core/html'
 import { type relement_env_T, type tag_dom_T, type wide_ctx_T } from 'relementjs'
 import { type tag_props_T } from 'relementjs/any'
@@ -6,11 +7,15 @@ import { dd_, div_, dl_, dt_ } from 'relementjs/html'
 import { md__raw_ } from '../md/index.js'
 export type dl_tree_props_T = {
 	ctx:wide_ctx_T
+	hyop?:string
 	dl_class?:string
+	div_class?:string
 	dt_class?:string
 	dd_class?:string
 	_?:(dl_tree_level:number)=>{
+		hyop?:string
 		dl_class?:string
+		div_class?:string
 		dt_class?:string
 		dd_class?:string
 	}
@@ -32,22 +37,26 @@ export function dl_tree_<env_T extends relement_env_T>(
 		if ((<dl_tree_props_T>props_OR_dt_dd_pair_a1_).ctx?.is_ctx) {
 			let {
 				ctx,
+				hyop,
 				dl_class,
+				div_class,
 				dt_class,
 				dd_class,
 				_
 			} = <dl_tree_props_T>props_OR_dt_dd_pair_a1_
 			if (_) {
 				const props2 = _(dl_tree_level)
+				if (props2) hyop = props2.hyop
 				dl_class = class_(dl_class, props2.dl_class)
+				div_class = class_(div_class, props2.div_class)
 				dt_class = class_(dt_class, props2.dt_class)
 				dd_class = class_(dd_class, props2.dd_class)
 			}
-			return dl_<env_T>({ class: dl_class }, dt_dd_pair_a1_!(dl_tree_level).map(([_dt, _dd])=>{
+			return dl_<env_T>({ class: dl_class, hyop }, dt_dd_pair_a1_!(dl_tree_level).map(([_dt, _dd])=>{
 				const _dt_a1 = <tag_dom_T[]>(<any[]>[_dt]).flat(Infinity)
 				const _dd_a1 = _dd ? <tag_dom_T[]>(<any[]>[_dd]).flat(Infinity) : null
 				return [
-					div_([
+					div_({ class: div_class }, [
 						dt_({ class: dt_class },
 							_dt_a1.map(__dt=>
 								typeof __dt === 'string'
