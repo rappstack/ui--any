88d54bc7862752552399c91f98085b1425688ff9
Brian Takita <brian.takita@gmail.com>
2024-05-08 20:07:05 -0400

    ./md: md__inline_raw_: arguments: + md__inline_raw_props_T

--- md/md__raw.ts
+++ md/md__raw.ts
@@ -1,6 +1,10 @@
 import './hljs.css'
 import { raw_, type relement_env_T, type wide_ctx_T } from 'relementjs'
 import { marked_ } from './marked.js'
+export type md__raw_props_T = {
+	ctx:wide_ctx_T
+	inline?:boolean
+}
 export function md__raw_<E extends relement_env_T>($p:md__raw_props_T, md:string) {
 	const { ctx, inline } = $p
 	return raw_<E>(
@@ -9,10 +13,9 @@ export function md__raw_<E extends relement_env_T>($p:md__raw_props_T, md:string
 		: marked_(ctx)!.parse(md)
 	)
 }
-export function md__inline_raw_<E extends relement_env_T>(ctx:wide_ctx_T, md:string) {
-	return raw_<E>(marked_(ctx)!.parseInline(md))
-}
-export type md__raw_props_T = {
+export type md__inline_raw_props_T = {
 	ctx:wide_ctx_T
-	inline?:boolean
+}
+export function md__inline_raw_<E extends relement_env_T>($p:md__inline_raw_props_T, md:string) {
+	return raw_<E>(marked_($p.ctx)!.parseInline(md))
 }
