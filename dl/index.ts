8037180a04e93c7da661debcffdb1a6a1d1e0930
Brian Takita <brian.takita@gmail.com>
2024-06-13 23:30:11 -0400

    ./dl: dl_tree_:
    	div_class→section_class
    	div_props→section_props
    	∋ div_→∋ section_

--- dl/index.ts
+++ dl/index.ts
@@ -2,15 +2,15 @@
 import { class_ } from 'ctx-core/html'
 import { type relement_env_T, type tag_dom_T, type wide_ctx_T } from 'relementjs'
 import { type tag_props_T } from 'relementjs/any'
-import { dd_, div_, dl_, dt_ } from 'relementjs/html'
+import { dd_, div_, dl_, dt_, section_ } from 'relementjs/html'
 import { md__raw_ } from '../md/index.js'
 import { nl } from '../string/index.js'
 export type dl_tree_basic_props_T = {
 	hyop?:string
 	dl_class?:string
 	dl_props?:Exclude<tag_props_T<HTMLElement>, 'class'>
-	div_class?:string
-	div_props?:Exclude<tag_props_T<HTMLElement>, 'class'>
+	section_class?:string
+	section_props?:Exclude<tag_props_T<HTMLElement>, 'class'>
 	dt_class?:string
 	dt_props?:Exclude<tag_props_T<HTMLElement>, 'class'>
 	dd_class?:string
@@ -40,8 +40,8 @@ export function dl_tree_<env_T extends relement_env_T>(
 				ctx,
 				dl_class,
 				dl_props,
-				div_class,
-				div_props,
+				section_class,
+				section_props,
 				dt_class,
 				dt_props,
 				dd_class,
@@ -52,8 +52,8 @@ export function dl_tree_<env_T extends relement_env_T>(
 				const props2 = _(dl_tree_level)
 				dl_class = class_(dl_class, props2.dl_class)
 				dl_props = { ...dl_props, ...props2.dl_props }
-				div_class = class_(div_class, props2.div_class)
-				div_props = { ...div_props, ...props2.div_props }
+				section_class = class_(section_class, props2.section_class)
+				section_props = { ...section_props, ...props2.section_props }
 				dt_class = class_(dt_class, props2.dt_class)
 				dt_props = { ...dt_props, ...props2.dt_props }
 				dd_class = class_(dd_class, props2.dd_class)
@@ -69,8 +69,8 @@ export function dl_tree_<env_T extends relement_env_T>(
 			if (basic_props) {
 				dl_class = class_(dl_class, basic_props.dl_class)
 				dl_props = { ...dl_props, ...basic_props.dl_props }
-				div_class = class_(div_class, basic_props.div_class)
-				div_props = { ...div_props, ...basic_props.div_props }
+				section_class = class_(section_class, basic_props.section_class)
+				section_props = { ...section_props, ...basic_props.section_props }
 				dt_class = class_(dt_class, basic_props.dt_class)
 				dt_props = { ...dt_props, ...basic_props.dt_props }
 				dd_class = class_(dd_class, basic_props.dd_class)
@@ -91,9 +91,9 @@ export function dl_tree_<env_T extends relement_env_T>(
 							? string__merge__a1_([dd_children])
 							: null
 					return [
-						div_({
-							...div_props,
-							class: div_class,
+						section_({
+							...section_props,
+							class: section_class,
 						}, [
 							dt_({
 								...dt_props,
@@ -130,8 +130,8 @@ export function dl_tree_<env_T extends relement_env_T>(
 			const {
 				dl_class,
 				dl_props,
-				div_class,
-				div_props,
+				section_class,
+				section_props,
 				dt_class,
 				dt_props,
 				dd_class,
@@ -146,8 +146,8 @@ export function dl_tree_<env_T extends relement_env_T>(
 				class: dl_class
 			}, dt_dd_pair_a1.map(([dt_children, dd_children])=>[
 				div_({
-					...div_props,
-					class: div_class,
+					...section_props,
+					class: section_class,
 				}, [
 					dt_({
 						...dt_props,
